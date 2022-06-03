ef0e12cbe767fe874624ed360aaa5418e5a70865
dhkimneer <kimds989@naver.com>
2022-06-03 20:13:19 +0900

    Chore: delete deploy.yml and change logo url

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -57,7 +57,7 @@ function Banner () {
     <>
       <Nav>
         <NavLink to='/'>
-          <img src="logo1.jpg" width="100"/>
+          <img src='logo1.png' width="100"/>
         </NavLink>
         <NavBtn>
           {
