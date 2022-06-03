cfcf55ba320f0ff35826faf776d0badcaf832bf1
Suziny <78532880+Suziny91@users.noreply.github.com>
2022-06-03 16:34:59 +0900

    Update Banner.js

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -57,7 +57,7 @@ function Banner () {
     <>
       <Nav>
         <NavLink to='/'>
-          <img src="logo1.png" width="50"/>
+          <img src="./logo1.png" width="100"/>
         </NavLink>
         <NavBtn>
           {
